60031b3e61f4b3ad64da52d3e8875ae439a00469
Anish Kataria <anish.21911105@viit.ac.in>
2025-02-13 12:52:29 -0800

    feat: 🎸 Added the updated code from old repo
    Added the updated code from the old laptop of different username.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Paste Bin App
+
+- A pastebin for sharing data without any authentication.
+- Go to some URl like "/pastebin/any"
+- Write anything, Save, Share the URL with anyone
+
+#### Made using React js and Firebase
+The PasteBinApp.js contains logic for the Pastebin. <br />
+The firebase.js file containg credentials for firebase (hidden) and some basic functions like save and get.
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import {BrowserRouter as Router, Switch, Route, Redirect, Link } from "react-router-dom";
+import './App.css';
+import PasteBinApp from './PasteBin/PasteBinApp';
+import PasteBinHome from "./PasteBin/PasteBinHome";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+
+          <Route exact path="/pastebin">
+              <PasteBinHome />
+          </Route>
+          <Route path="/pastebin">
+              <PasteBinApp />
+          </Route>
+
+          {/* <Route exact path="/canvas">
+              <CanvasHome />
+          </Route>
+          <Route path="/canvas">
+              <CanvasApp />
+          </Route> */}
+
+          <Route path="/">
+            <Link to="/pastebin">
+              <div style={{fontSize: "100px"}}>
+                Go to pastebin
+              </div>
+            </Link>
+            {/* <Link to="/canvas">
+              <div style={{fontSize: "100px"}}>
+                Go to canvas
+              </div>
+            </Link> */}
+              {/* {window.location.pathname = "/pastebin"} */}
+          </Route>
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
